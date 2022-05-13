1978f69e8a1736b36cc748abe2cd86dad74e027d
akhremYuri <akhremr@gmail.com>
2022-05-13 12:19:33 +0300

    feat: add animation functionality (Fri, May 13, 2022 12:19:35)

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -20,17 +20,40 @@ const sliderProps = [
 const slide = document.querySelector("#slide");
 const sliderDesc = document.querySelector("#slide-description");
 const dotsContainer = document.querySelector("#dots-container");
+let slideId = 1;
 
 function dotClick(event) {
   const activeSlide = document.querySelector(".dot.active");
   if (activeSlide) {
     activeSlide.classList.remove("active");
   }
-  const slideId = +event.target.dataset.id;
+  slideId = +event.target.dataset.id;
+  event.target.classList.add("active");
+  sliderDesc.classList.add("slidein");
+  slide.classList.add("opacityoff");
+}
+
+function slideDescAnimationIteration() {
+  sliderDesc.innerText = sliderProps[slideId - 1].desc;
+}
+
+function slideAnimationIteration() {
   slide.setAttribute("src", `./assets/${sliderProps[slideId - 1].img}`);
   slide.setAttribute("alt", sliderProps[slideId - 1].desc);
-  sliderDesc.innerText = sliderProps[slideId - 1].desc;
-  event.target.classList.add("active");
+}
+
+function sliderDescClassRemove() {
+  if (sliderDesc.classList.contains("slidein"))
+    sliderDesc.classList.remove("slidein");
+}
+
+function slideOffEnd() {
+  if (slide.classList.contains("opacityoff"))
+    slide.classList.remove("opacityoff");
 }
 
 dotsContainer.addEventListener("click", dotClick);
+sliderDesc.addEventListener("animationend", sliderDescClassRemove);
+sliderDesc.addEventListener("animationiteration", slideDescAnimationIteration);
+slide.addEventListener("animationend", slideOffEnd);
+slide.addEventListener("animationiteration", slideAnimationIteration);
